a112cbdee0c45f672a9d2490b01464c3a5e7a742
rhdmAdmin <>
2019-04-05 10:18:08 -0400

    {/src/main/java/com/myspace/cucumber_test_project/Transaction.java}

--- src/main/java/com/myspace/cucumber_test_project/Transaction.java
+++ src/main/java/com/myspace/cucumber_test_project/Transaction.java
@@ -12,6 +12,8 @@ public class Transaction implements java.io.Serializable {
 	private boolean foreignCurrencyTransaction;
 	private java.lang.Float transactionAmount;
 
+	private java.lang.Boolean needReview;
+
 	public Transaction() {
 	}
 
@@ -39,12 +41,21 @@ public class Transaction implements java.io.Serializable {
 		this.transactionAmount = transactionAmount;
 	}
 
+	public java.lang.Boolean getNeedReview() {
+		return this.needReview;
+	}
+
+	public void setNeedReview(java.lang.Boolean needReview) {
+		this.needReview = needReview;
+	}
+
 	public Transaction(java.lang.String transactionRefNum,
 			boolean foreignCurrencyTransaction,
-			java.lang.Float transactionAmount) {
+			java.lang.Float transactionAmount, java.lang.Boolean needReview) {
 		this.transactionRefNum = transactionRefNum;
 		this.foreignCurrencyTransaction = foreignCurrencyTransaction;
 		this.transactionAmount = transactionAmount;
+		this.needReview = needReview;
 	}
 
 }
